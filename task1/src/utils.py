98f3f242551a1424828458962490f2b4d432e3d5
halungge <magdalena@umsu.de>
2017-04-14 12:17:26 +0200

    add padding

--- task1/src/utils.py
+++ task1/src/utils.py
@@ -8,13 +8,12 @@ class SentenceCleaner:
     SPLIT = " "
 
     def prepare_sentence(self, input_string):
+        words = input_string.split(self.SPLIT)
+        t = [words[i] if i < len(words) else self.PADDING for i in range(30)]
         line_array = np.ndarray([30], dtype='object')
         line_array[0] = self.INIT_SEQ
-        word_list = input_string.split(self.SPLIT)
-        end = min(len(word_list), self.LENGTH-1)
-        line_array[1:end + 1] = word_list[0:end]
+        line_array[1:self.LENGTH] = t[0:self.LENGTH-1]
         line_array[-1] = self.END_SEQ
-
         return line_array
 
 
--- task1/test/test_utils.py
+++ task1/test/test_utils.py
@@ -4,32 +4,31 @@ import unittest
 from utils import SentenceCleaner
 
 class SentenceCleanerTest(unittest.TestCase):
+     cleaner = SentenceCleaner()
 
      if __name__ == '__main__':
           unittest.main()
 
-     def test_SentenceCleaner_returns_array_with_shape_30(self):
-          cleaner = SentenceCleaner()
-          sentence = "lorem ipsum dolor sit amet , consetetur"
-          append_split = cleaner.prepare_sentence(sentence)
-
-          assert append_split.shape[0] == cleaner.LENGTH
-          assert append_split[-1] == cleaner.END_SEQ
-
-
      def test_SentenceCleaner_array_starts_with_bos(self):
           sentence = "lorem ipsum dolor sit amet , consetetur"
-          cleaner = SentenceCleaner()
-          append_split = cleaner.prepare_sentence(sentence)
-
-          assert append_split[0] == cleaner.INIT_SEQ
-          assert append_split[-1] == cleaner.END_SEQ
-
+          append_split = self.cleaner.prepare_sentence(sentence)
+          assert append_split[0] == self.cleaner.INIT_SEQ
 
      def test_SentenceCleaner_array_ends_with_is_eos(self):
           sentence = "lorem ipsum dolor sit amet , consetetur"
-          cleaner = SentenceCleaner()
-          append_split = cleaner.prepare_sentence(sentence)
-          assert append_split[-1] == cleaner.END_SEQ
+          append_split = self.cleaner.prepare_sentence(sentence)
+          assert append_split[-1] == self.cleaner.END_SEQ
+
+     def test_SentenceCleaner_returns_array_with_shape_30(self):
+          sentence = "Aliquam ut mattis felis , vel accumsan orci . Donec arcu dolor , luctus in nibh id , rutrum consequat elit . Phasellus sed dolor maximus , euismod quam sed , varius tellus . Fusce condimentum libero in ante lobortis"
+          append_split = self.cleaner.prepare_sentence(sentence)
+          assert append_split.shape[0] == self.cleaner.LENGTH
+
+
+     def test_array_is_padded(self):
+          sentence = "lorem ipsum dolor sit amet , consetetur"
+          ar = self.cleaner.prepare_sentence(sentence)
+          for i in range(8,29):
+               assert ar[i] == self.cleaner.PADDING
 
 
