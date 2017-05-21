a3b529bbf5fa2cafe9d3ec4fd299191acfd40b59
halungge <magdalena@umsu.de>
2017-05-21 13:55:11 +0200

    write vocabulary to file add yml configuration file for training

--- task2/prepare_training_data.sh
+++ task2/prepare_training_data.sh
@@ -5,16 +5,21 @@ TRAIN_INPUT=$DATA_PATH/Training_Shuffled_Dataset.txt
 VALIDATION_INPUT=$DATA_PATH/Validation_Shuffled_Dataset.txt
 SEQ2SEQ_TOOLS=seq2seq/bin/tools/
 
+echo " split triplets in training data"
 python ./split_triplets.py $TRAIN_INPUT
 
+echo "split triplets in validation data"
 python ./split_triplets.py $VALIDATION_INPUT
 
 
 #generate the vocabular from the source file (contains the same data in terms of words as the target file...)
-#use MAX_VOCAB_SIZE=10000 as this is the vocabulary size used in the data (see Readme.txt)
 MAX_VOCAB_SIZE=20000
+VOCABULARY=$DATA_PATH/vocabulary_$MAX_VOCAB_SIZE.txt
 MIN_FREQUENCY=0
+
+echo "generating vocabulary with size $MAX_VOCAB_SIZE from training data"
 #
-python3 $SEQ2SEQ_TOOLS/generate_vocab.py --max_vocab_size $MAX_VOCAB_SIZE --min_frequency $MIN_FREQUENCY $DATA_PATH/Training_Shuffled_Dataset_source.txt
+python3 $SEQ2SEQ_TOOLS/generate_vocab.py --max_vocab_size $MAX_VOCAB_SIZE --min_frequency $MIN_FREQUENCY $DATA_PATH/Training_Shuffled_Dataset_source.txt > $VOCABULARY
+
 
 
