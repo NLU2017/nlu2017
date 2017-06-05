c37e1ce49b501415d5604926fdc3309bf6809234
Oliver Burkhard <oliver.burkhard@gmail.com>
2017-06-05 12:54:35 +0200

    first (non working, alas) attempt at a two-to-one-encoder.

---
+++ task2/run_improved_training_baseline.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+export BASE_DIR=$(pwd)
+export SEQ2SEQ_PATH=${BASE_DIR}/seq2seq
+export DATA_DIR=${BASE_DIR}/data
+export VOCAB_SOURCE=${DATA_DIR}/vocabulary_20000.txt
+export VOCAB_TARGET=${DATA_DIR}/vocabulary_20000.txt
+export TRAIN_SOURCES1=${DATA_DIR}/Training_Shuffled_Dataset_source1.txt
+export TRAIN_SOURCES2=${DATA_DIR}/Training_Shuffled_Dataset_source2.txt
+export TRAIN_TARGETS=${DATA_DIR}/Training_Shuffled_Dataset_target.txt
+export DEV_SOURCES1=${DATA_DIR}/Validation_Shuffled_Dataset_source1.txt
+export DEV_SOURCES2=${DATA_DIR}/Validation_Shuffled_Dataset_source2.txt
+export DEV_TARGETS=${DATA_DIR}/Validation_Shuffled_Dataset_target.txt
+
+export DEV_TARGETS_REF=${DATA_DIR}/Validation_Shuffled_Dataset_target.txt
+export TRAIN_STEPS=32000
+export MODEL_DIR=${BASE_DIR}/runs/baseline_improved
+
+mkdir -p $MODEL_DIR
+
+
+python3 ${SEQ2SEQ_PATH}/bin/train.py \
+    --config_paths="
+      ./train_improved_baseline.yml,
+      ${SEQ2SEQ_PATH}/example_configs/train_seq2seq.yml"\
+  --model_params "
+      vocab_source: $VOCAB_SOURCE
+      vocab_target: $VOCAB_TARGET" \
+  --input_pipeline_train "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - [$TRAIN_SOURCES1, $TRAIN_SOURCES2]
+      target_files:
+        - $TRAIN_TARGETS" \
+  --input_pipeline_dev "
+    class: ParallelTextInputPipeline
+    params:
+       source_files:
+        - $DEV_SOURCES1
+        - $DEV_SOURCES2
+       target_files:
+        - $DEV_TARGETS" \
+  --batch_size 32 \
+  --train_steps $TRAIN_STEPS \
+  --output_dir $MODEL_DIR
+
+echo "done"
+
