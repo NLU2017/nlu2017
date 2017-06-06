2c2db12962e6a6a093688bf1eb079dc9760c124f
halungge <magdalena@umsu.de>
2017-06-06 13:27:19 +0200

    add prediciton script for attention net

---
+++ task2/predict_attention.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+export BASE_DIR=$(pwd)
+export SEQ2SEQ_PATH=${BASE_DIR}/seq2seq
+export DATA_DIR=${BASE_DIR}/data
+export VOCAB_SOURCE=${DATA_DIR}/vocabulary_20000.txt
+export VOCAB_TARGET=${DATA_DIR}/vocabulary_20000.txt
+export TRAIN_SOURCES=${DATA_DIR}/Training_Shuffled_Dataset_source.txt
+export TRAIN_TARGETS=${DATA_DIR}/Training_Shuffled_Dataset_target.txt
+export DEV_SOURCES=${DATA_DIR}/Validation_Shuffled_Dataset_source.txt
+export DEV_TARGETS=${DATA_DIR}/Validation_Shuffled_Dataset_target.txt
+
+export DEV_TARGETS_REF=${DATA_DIR}/Validation_Shuffled_Dataset_target.txt
+
+export MODEL_DIR=${BASE_DIR}/runs/baseline
+
+export PRED_DIR=${MODEL_DIR}/pred
+mkdir -p ${PRED_DIR}
+
+python3 ${SEQ2SEQ_PATH}/bin/infer.py \
+  --tasks "
+    - class: DecodeText
+        params:
+            unk_replace:True
+    - class: DumpBeams
+      params:
+        file: ${PRED_DIR}/beams.npz
+    - class: DumpAttention
+        params:
+            output_dir: $PRED_DIR/attention"
+  --model_dir $MODEL_DIR \
+  --model_params "
+    inference.beam_search.beam_width: 5" \
+  --input_pipeline "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - $DEV_SOURCES" \
+  >  ${PRED_DIR}/predictions_baseline.txt
+
+./bin/tools/multi-bleu.perl ${DEV_TARGETS_REF} < ${PRED_DIR}/predictions.txt
